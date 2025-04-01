4477a99606e8390a445f006edcb03556f50d4da6
Laura Mariana de Araujo Ferreira <lmaf@NEUROTECH.INTRANET>
2025-04-01 16:01:05 -0300

    123

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,16 @@
+//Primeiro programa em JavaScript
+//Mostra mensagem ao abrir 
+alert("Boas vindas ao nosso site!");
+//Recebe as informações de nome e idade
+let nome = prompt("Digite seu Nome");
+let idade = prompt("Digite sua Idade");
+//Valida se a pessoa é menos de idade, se não está apta
+if(idade < 18){
+    let mensagem = prompt("Erro! Pessoa de menor!")
+}else{
+    let mensagem = prompt("Pode tirar a habilitação!.")
+}
+
+
+
+
